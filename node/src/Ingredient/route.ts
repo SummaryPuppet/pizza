db6c30e3bbacf77ec138005296e55c4d5d3fae53
SummaryPuppet <adrianm_sv@hotmail.com>
2024-01-24 00:18:32 -0500

    add TSDocs for ingredient and product routes

--- node/src/Ingredient/route.ts
+++ node/src/Ingredient/route.ts
@@ -11,16 +11,46 @@ import {
 
 const router = Router();
 
+/**
+ * Get all Ingredients
+ * @route GET /ingredients
+ * @returns Ingredient[] - List of ingredients
+ */
 router.get("/ingredients", getAllIngredients);
 
+/**
+ * Get an Ingredient from id
+ * @route GET /ingredient/:id
+ * @returns Ingredient
+ */
 router.get("/ingredient/:id", getIngredient);
 
+/**
+ * Create an Ingredient
+ * @route POST /ingredient
+ * @returns Ingredient
+ */
 router.post("/ingredient", upload.single("ingredientImg"), createIngredient);
 
+/**
+ * Update an Ingredient from id
+ * @route PUT /ingredient/:id
+ * @returns Ingredient
+ */
 router.put("/ingredient/:id", upload.single("ingredientImg"), updateIngredient);
 
+/**
+ * Update the quantity fon an Ingredient from id
+ * @route PUT /ingredient/:id/quantity
+ * @returns Ingredient
+ */
 router.put("/ingredient/:id/quantity", updateIngredientQuantity);
 
+/**
+ * Delete an Ingredient from id
+ * @route DELETE /ingredient/:id
+ * @returns Ingredient
+ */
 router.delete("/ingredient/:id", deleteIngredient);
 
 export default router;
--- node/src/Product/route.ts
+++ node/src/Product/route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import upload from "../storage";
 import {
   createProduct,
   deleteProduct,
@@ -8,20 +9,49 @@ import {
   updateProduct,
   updateProductQuantity,
 } from "./controller";
-import upload from "../storage";
 
 const router = Router();
 
+/**
+ * Get all Products
+ * @route GET /products
+ * @returns Product[] - List of products
+ */
 router.get("/products", getAllProducts);
 
+/**
+ * Get a Product from id
+ * @route GET /product/:id
+ * @returns Product
+ */
 router.get("/product/:id", getProduct);
 
+/**
+ * Create a Product
+ * @route POST /product/:id
+ * @returns Product
+ */
 router.post("/product", upload.single("productImg"), createProduct);
 
+/**
+ * Update a Product from id
+ * @route PUT /product/:id
+ * @returns Product
+ */
 router.put("/product/:id", upload.single("productImg"), updateProduct);
 
+/**
+ * Update the quantity fon a Product from id
+ * @route PUT /product/:id
+ * @returns Product
+ */
 router.put("/product/:id/quantity", updateProductQuantity);
 
+/**
+ * Delete Product from id
+ * @route DELETE /product/:id
+ * @returns Product
+ */
 router.delete("/product/:id", deleteProduct);
 
 export default router;
